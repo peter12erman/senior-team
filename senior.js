4b43656973bfc3a57e7601d5531f10425c515133
VaxoRuxadze <vr12ruxadze@gmail.com>
2025-03-28 12:21:58 +0400

    forth_change

--- senior.js
+++ senior.js
@@ -5,3 +5,8 @@ console.log("Through heaven and earth i alone am the honored one.")
 console.log("Its Raining outside")
 
 console.log("Its Cold Outside.")
+
+function change() {
+    console.log("Its Blizzard ")
+    return 100
+}
